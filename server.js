cf109c5a32461b1e222eb27929034fe1b25e271c
yolointern <yolointern@yolohealth.in>
2019-06-24 18:10:01 +0530

    initialize with the nodejs and express and mongodb

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ var app 		= express();
 const port 		= 8000 || process.env.PORT;
 var morgan 		= require('morgan');
 var mongoose 	= require('mongoose');
-var User 		= require('./app/models/user');
 var bodyParser	= require('body-parser');
+var router		= express.Router();
+var appRoutes 	= require('./app/routes/api')(router);
+var path		= require('path');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use('/api',appRoutes);
+//http://localhost:8000/api/users
 
 mongoose.connect('mongodb+srv://bhumesh1998:bhumesh1998@cluster0-ubi02.mongodb.net/test?retryWrites=true&w=majority',function(err){
 	if(err){
@@ -20,26 +25,9 @@ mongoose.connect('mongodb+srv://bhumesh1998:bhumesh1998@cluster0-ubi02.mongodb.n
 	}
 });
 
-app.post('/users', function(req,res){
-	var user = new User();
-	user.username = req.body.username;
-	user.password = req.body.password;
-	user.email = req.body.email;
-	if(req.body.username == null || req.body.password == null || req.body.email == null ||req.body.username == '' || req.body.password == '' || req.body.email == '' ){
-		res.send('Ensure username, email, and password were provided');
-	}
-	else{
-		user.save(function(err){
-			if(err) {
-				res.send("username or email is already exists !");
-			}
-			else{
-				res.send("user created !");
-			}
-		});
-	}
-});
-
+app.get('*', function (req,res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'))
+})
 app.listen(port ,function() {
 	console.log("Running server at port : ",port);
 }); 
